52118f7e98ed7f38b8eda7086bf4999f450d9177
0xantman <antho74830@gmail.com>
2024-04-30 12:08:14 +0200

    Fix path

--- src/index.ts
+++ src/index.ts
@@ -6,45 +6,24 @@ import {Connection} from "@solana/web3.js";
 import fs from "fs";
 const {BN} = anchor;
 import { Keypair } from '@solana/web3.js';
-
-
-
+import {getProgram, loadWallet, initAccounts} from "./utils/connection.js";
 
 async function update_event(){
-    // @ts-ignore
-    const keypairJSON = fs.readFileSync(process.env.WALLET, 'utf-8');
-    const keypairData = JSON.parse(keypairJSON);
-    const keypair = Keypair.fromSecretKey(Uint8Array.from(keypairData));
-    const wallet = new anchor.Wallet(keypair);
-    // Create a connection to the Solana blockchain
-    // @ts-ignore
-    const connection =  new Connection(process.env.PRIVATE_SOLANA_NETWORK_URL, 'processed');
-    // Set the provider with only the connection
-    // @ts-ignore
-    const provider = new anchor.AnchorProvider(connection, wallet, {'preflightCommitment': 'processed'});
-    anchor.setProvider(provider);
+    const wallet = loadWallet();
+    const programId = new anchor.web3.PublicKey(process.env.NEXT_PUBLIC_BATTLEBOOSTERS_PROGRAM_ID!);
+    const program = getProgram(wallet, programId) as anchor.Program<Battleboosters>;
 
-    const idlJSON = fs.readFileSync(process.cwd() + '/src/battleboosters.json', 'utf-8');
-
-    const idl = JSON.parse(idlJSON);
-    // @ts-ignore
-    const programId = new anchor.web3.PublicKey(process.env.NEXT_PUBLIC_BATTLEBOOSTERS_PROGRAM_ID);
-    // 2. Create program instance
-    const program: anchor.Program<Battleboosters> = new anchor.Program(idl, programId, provider);
-
-    // const {
-    //     admin_account,
-    //     unauthorized_account,
-    //     metadata_pubkey,
-    //     bank_pda,
-    //     bank_bump,
-    //     program_pda,
-    //     program_bump,
-    //     rarity_pda,
-    //     rarity_bump,
-    //     mint_authority_account,
-    //     authority_bump,
-    // } = initAccounts(program);
+    const {
+        metadata_pubkey,
+        bank_pda,
+        bank_bump,
+        program_pda,
+        program_bump,
+        rarity_pda,
+        rarity_bump,
+        mint_authority_account,
+        authority_bump,
+    } = initAccounts(program);
 
     const event_id = 1;
     const new_time_start = 1813535498;
---
+++ src/utils/connection.ts
@@ -0,0 +1,77 @@
+// Load environment variables (assuming you have a .env file)
+import dotenv from 'dotenv';
+dotenv.config();
+
+import * as anchor from '@coral-xyz/anchor';
+import { Connection, Keypair } from '@solana/web3.js';
+import fs from 'fs';
+import { MPL_TOKEN_METADATA_PROGRAM_ID } from '@metaplex-foundation/mpl-token-metadata';
+import {Battleboosters} from "../battleboosters";
+
+
+// Function to load the wallet from a JSON file
+export function loadWallet(): anchor.Wallet {
+    const keypairJSON = fs.readFileSync(process.env.WALLET_PATH!, 'utf-8');
+    const keypairData = JSON.parse(keypairJSON);
+    const keypair = Keypair.fromSecretKey(Uint8Array.from(keypairData));
+    return new anchor.Wallet(keypair);
+}
+
+// Function to create a program instance
+export function getProgram(wallet: anchor.Wallet, programId: anchor.web3.PublicKey): anchor.Program<Battleboosters> {
+
+    const connection = new Connection(process.env.PRIVATE_SOLANA_NETWORK_URL!, 'processed');
+    const provider = new anchor.AnchorProvider(connection, wallet, {
+        preflightCommitment: 'processed',
+    });
+    anchor.setProvider(provider);
+
+    // Load the program's IDL
+    const idlJSON = fs.readFileSync(process.cwd() + '/src/battleboosters.json', 'utf-8');
+    const idl = JSON.parse(idlJSON);
+
+    return new anchor.Program(idl, programId, provider);
+}
+
+export function initAccounts (program: anchor.Program<Battleboosters>) {
+
+    const metadata_pubkey = new anchor.web3.PublicKey(
+        MPL_TOKEN_METADATA_PROGRAM_ID
+    );
+    const [bank_pda, bank_bump] = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from('BattleBoosters'), Buffer.from('bank')],
+        program.programId
+    );
+
+    const [program_pda, program_bump] =
+        anchor.web3.PublicKey.findProgramAddressSync(
+            [Buffer.from('BattleBoosters'), Buffer.from('program')],
+            program.programId
+        );
+
+    const [rarity_pda, rarity_bump] =
+        anchor.web3.PublicKey.findProgramAddressSync(
+            [Buffer.from('BattleBoosters'), Buffer.from('rarity')],
+            program.programId
+        );
+
+    const [mint_authority_account, authority_bump] =
+        anchor.web3.PublicKey.findProgramAddressSync(
+            [Buffer.from('BattleBoosters'), Buffer.from('mintAuthority')],
+            program.programId
+        );
+
+    return {
+        metadata_pubkey,
+        bank_pda,
+        bank_bump,
+        program_pda,
+        program_bump,
+        rarity_pda,
+        rarity_bump,
+        mint_authority_account,
+        authority_bump,
+    };
+
+};
+
