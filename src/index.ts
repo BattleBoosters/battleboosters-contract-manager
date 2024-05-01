64fae03d674beb5169ebbb0e80fff7b06aa99af7
0xantman <antho74830@gmail.com>
2024-05-01 17:02:41 +0200

    Update cli with create event

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 import {program} from "commander"
 import { start } from "./utils/start.js"
-import { updateEvent } from "./utils/event.js"
+import {createEvent, updateEvent} from "./utils/event.js"
 import fs from "fs";
 import {TournamentType, RankReward} from "./interfaces/interfaces";
 
@@ -19,18 +19,73 @@ program
         });
     });
 
+
 program
     .command('create-event')
-    .description('Create a new event')
-    .argument('<event_id>', 'ID of the event')
+    .description('Create an event')
     .argument('<start_time>', 'Start time of the event')
     .argument('<end_time>', 'End time of the event')
-    .action((eventId: any, startTime: any, endTime: any) => {
-        // ... Implement your create-event logic here
-        console.log('Creating event with ID:', eventId);
-        // ... Use startTime and endTime as needed
-    });
+    .option('--main-card', 'Set tournament type to main card')
+    .option('--prelims', 'Set tournament type to prelims')
+    .option('--early-prelims', 'Set tournament type to early prelims')
+    .option('--rank-config <path>', 'Path to rank reward configuration file')
+    .action(async (startTime, endTime, options) => {
+        // ... Implement update-event logic
+        let tournamentType: TournamentType =  { prelims: {} }
+        if (options.prelims) {
+            tournamentType = { prelims: {} };
+        } else if (options.mainCard) {
+            tournamentType = { mainCard: {} };
+        }else if (options.earlyPrelims){
+            tournamentType = { earlyPrelims: {} };
+        }else {
+            // Handle the case where no option is selected or provide a default
+            console.error('Error: Please specify a tournament type using --main-card, --prelims, or --early-prelims');
+            return; // Exit the action if no type is provided
+        }
 
+
+        let rankRewards: RankReward[] = [];
+        if (options.rankConfig) {
+            try {
+                const configFile = fs.readFileSync(options.rankConfig, 'utf-8'); // Specify encoding
+                const parsedData = JSON.parse(configFile);
+
+                // Validate if parsedData is an array of RankReward objects
+                if (Array.isArray(parsedData) && parsedData.every(item => isRankReward(item))) {
+                    rankRewards = parsedData;
+                } else {
+                    throw new Error('Invalid rank configuration file: Data does not conform to RankReward structure.');
+                }
+            } catch (error) {
+                console.error('Error reading or parsing rank configuration file:', error);
+                // Handle error appropriately (e.g., exit process or prompt for a valid file)
+            }
+        }
+
+        // Type guard function to check if an object is a RankReward
+        function isRankReward(obj: any): obj is RankReward {
+            return (
+                typeof obj.startRank === 'number' &&
+                (typeof obj.endRank === 'number' || obj.endRank === null) &&
+                typeof obj.prizeAmount === 'number' &&
+                typeof obj.fighterAmount === 'number' &&
+                typeof obj.boosterAmount === 'number' &&
+                typeof obj.championsPassAmount === 'number'
+            );
+        }
+
+
+
+        const { tx } = await createEvent(
+            startTime,
+            endTime,
+            tournamentType,
+            rankRewards
+        );
+
+        console.log('Event created: ',tx);
+    });
 
 program
     .command('update-event')
@@ -90,14 +145,14 @@ program
 
 
 
-        await updateEvent(
+        const { tx } = await updateEvent(
             eventAccount,
             startTime,
             endTime,
             tournamentType,
             rankRewards
         );
-        console.log('Event updated:', eventAccount);
+        console.log('Event updated: ', eventAccount + " tx "+ tx);
     });
 
 
--- src/utils/event.ts
+++ src/utils/event.ts
@@ -4,10 +4,58 @@ import {Battleboosters} from "../battleboosters";
 import {RankReward, TournamentType} from "../interfaces/interfaces";
 const {BN} = anchor;
 
-const createEvent = async () => {
-    /*
-        TODO: This method will serve create an event
-     */
+
+const createEvent = async (time_start: number, time_end: number, tournament_type: TournamentType, rank_rewards: RankReward[]) => {
+    const wallet = loadWallet();
+    const programId = new anchor.web3.PublicKey(process.env.NEXT_PUBLIC_BATTLEBOOSTERS_PROGRAM_ID!);
+    const program = getProgram(wallet, programId) as anchor.Program<Battleboosters>;
+    const {
+        program_pda,
+    } = initAccounts(program);
+
+    const program_data = await program.account.programData.fetch(
+        program_pda
+    );
+    const [event_account, event_account_bump] =
+        anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from('BattleBoosters'),
+                Buffer.from('event'),
+                new BN(program_data.eventNonce).toBuffer('le', 8),
+            ],
+            program.programId
+        );
+
+    let updated_rank_rewards: RankReward[] = []
+    rank_rewards.map(el => {
+        updated_rank_rewards.push({
+            startRank: new BN(el.startRank),
+            endRank: el.endRank ? new BN(el.endRank): null,
+            prizeAmount: el.prizeAmount,
+            fighterAmount: el.fighterAmount,
+            boosterAmount: el.boosterAmount,
+            championsPassAmount: el.championsPassAmount
+        })
+    })
+    const tx = await program.methods
+        .createNewEvent(
+            new BN(time_start),
+            new BN(time_end),
+            tournament_type,
+            updated_rank_rewards
+        )
+        .accounts({
+            creator: wallet.publicKey,
+            program: program_pda,
+            event: event_account,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([])
+        .rpc();
+
+    return {
+        tx
+    }
 }
 
 const updateEvent = async (event_account: string, time_start: number, time_end: number, tournament_type: TournamentType, rank_rewards: RankReward[]) => {
@@ -47,6 +95,9 @@ const updateEvent = async (event_account: string, time_start: number, time_end: 
         .signers([])
         .rpc();
 
+    return {
+        tx
+    }
 }
 
 export { createEvent, updateEvent }
